c1c0bcbb15c91cc866836c78487206cf783b7c89
michael47265 <michael47265@yahoo.com>
2024-04-26 17:21:16 -0400

    unit test project

---
+++ testProject2/ShowCase/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UtilityLibraries;
+
+public class Program{
+    public static void Main(string[] args){
+        bool cont = true;
+
+
+        
+        string word;
+        //StringLibrary check = new StringLibrary();
+        do{
+        Console.WriteLine("Enter a word");
+        word = Console.ReadLine();
+        //check.IsCapital(word);
+        Console.WriteLine($"{word} first letter is capital {(word.StartsWithUpper() ? "Y":"N")}");
+        Console.WriteLine("do you want to continue y/n");
+        
+        if(Console.ReadLine().ToLower()=="y")
+        cont=true;
+        else cont = false;
+
+        }while(cont);
+    }
+}
---
+++ testProject2/StringLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace UtilityLibraries;
+
+public static class StringLibrary{
+
+
+    public static bool StartsWithUpper(this string? str){
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
