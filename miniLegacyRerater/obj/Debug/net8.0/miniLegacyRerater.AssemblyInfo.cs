8e29e4205b81d4b6541b23d09cb6752eae914c30
michael47265 <michael47265@yahoo.com>
2024-05-16 15:39:54 -0400

    main changes

--- miniLegacyRerater/obj/Debug/net8.0/miniLegacyRerater.AssemblyInfo.cs
+++ miniLegacyRerater/obj/Debug/net8.0/miniLegacyRerater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("miniLegacyRerater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc48e25488f79839305e399d6fb718ed2f23b53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7512c42ab9fdce9568311b485cf329f8aac2e190")]
 [assembly: System.Reflection.AssemblyProductAttribute("miniLegacyRerater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("miniLegacyRerater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
